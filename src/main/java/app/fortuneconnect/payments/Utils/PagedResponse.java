4dfcc752b0fc900686a1b786c7d6d6f646ebd357
mwash-fred <mwangiwilly395@gmail.com>
2023-04-26 13:02:47 +0300

    Repaginate

--- src/main/java/app/fortuneconnect/payments/Utils/PagedResponse.java
+++ src/main/java/app/fortuneconnect/payments/Utils/PagedResponse.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.hateoas.Link;
 
 import java.util.List;
-@Data @Builder
+@Data @NoArgsConstructor
 public class PagedResponse<T> {
     private List<T> content;
     private int page;
@@ -16,14 +16,12 @@ public class PagedResponse<T> {
     List<Link> links;
     private String baseUrl;
 
-    public PagedResponse() {
-    }
-
-    public PagedResponse(List<T> content, int currentPageNumber, int pageSize, long totalElements, List<Link> links, String baseUrl) {
+    public PagedResponse(List<T> content, int currentPageNumber, int pageSize, long totalElements, int totalPages, List<Link> links, String baseUrl) {
         this.content = content;
         this.page = currentPageNumber;
         this.size = pageSize;
         this.totalElements = totalElements;
+        this.totalPages = totalPages;
         this.links = links;
         this.baseUrl = baseUrl;
     }
