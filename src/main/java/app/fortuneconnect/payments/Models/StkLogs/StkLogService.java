71526f33ede54ccad0177d605db72688c2a5998a
mwash-fred <mwangiwilly395@gmail.com>
2023-05-17 18:25:34 +0300

    Logs

--- src/main/java/app/fortuneconnect/payments/Models/StkLogs/StkLogService.java
+++ src/main/java/app/fortuneconnect/payments/Models/StkLogs/StkLogService.java
@@ -1,9 +1,7 @@
 package app.fortuneconnect.payments.Models.StkLogs;
 
 import app.fortuneconnect.payments.DTO.Responses.StkCallbackResponseBody;
-import app.fortuneconnect.payments.DTO.Responses.StkCallbackResponseDTO;
 import app.fortuneconnect.payments.Utils.Const.MpesaStaticStrings;
-import app.fortuneconnect.payments.Utils.MpesaActions;
 import app.fortuneconnect.payments.Utils.StringToDateConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +12,9 @@ import java.text.ParseException;
 public class StkLogService implements StkLogOperations{
 
     private  final StkLogRepository stkLogRepository;
-    private final MpesaActions actions;
 
-    public StkLogService(StkLogRepository stkLogRepository, MpesaActions mpesaActions){
+    public StkLogService(StkLogRepository stkLogRepository){
         this.stkLogRepository = stkLogRepository;
-        this.actions = mpesaActions;
     }
 
     @Override
@@ -33,29 +29,28 @@ public class StkLogService implements StkLogOperations{
 
     @Transactional
     @Override
-    public StkLog updateLog(StkCallbackResponseDTO callback) {
-        StkLog log = retriveByMerchantId(callback.getBody().getStkCallback().getMerchantRequestID());
-        if(callback.getBody().getStkCallback().getResultCode() == 0){
-            callback.getBody().getStkCallback().getCallbackMetadata().getItem().forEach(
+    public StkLog updateLog(StkCallbackResponseBody callback) {
+        StkLog log = retriveByMerchantId(callback.getStkCallback().getMerchantRequestID());
+        if(callback.getStkCallback().getResultCode() == 0){
+            callback.getStkCallback().getCallbackMetadata().getItem().forEach(
                     item -> {
                         switch (item.getName()){
-                            case MpesaStaticStrings.MPESA_RECEIPT_NO -> log.getMpesaPayment().setMpesaTransactionNo(item.getValue().toString());
+                            case MpesaStaticStrings.MPESA_RECEIPT_NO -> log.getMpesaPayment().setMpesaTransactionNo((String) item.getValue());
                             case MpesaStaticStrings.AMOUNT -> log.getMpesaPayment().setTransactionAmount((Double) item.getValue());
                             case MpesaStaticStrings.TRANSACTION_DATE -> {
                                 try {
-                                    log.getMpesaPayment().setTransactionTime(StringToDateConverter.parse(item.getValue().toString()));
+                                    log.getMpesaPayment().setTransactionTime(StringToDateConverter.parse((String) item.getValue()));
                                 } catch (ParseException e) {
                                     throw new RuntimeException(e);
                                 }
                             }
                             case MpesaStaticStrings.BALANCE -> {}
-                            default -> log.getMpesaPayment().setPhoneNumber(item.getValue().toString());
+                            default -> log.getMpesaPayment().setPhoneNumber((String) item.getValue());
                         }
                     }
             );
             log.getMpesaPayment().setTransactionStatus(true);
         }
-        actions.callBackWithConfirmationOrFailure(log.getMpesaPayment().getAccountNo(),log.getMpesaPayment().getTransactionAmount(), log.getMpesaPayment().getMpesaTransactionNo(), log.getCallbackUrl());
         return stkLogRepository.save(log);
     }
 
